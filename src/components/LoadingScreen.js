9728c40815c8614542dd001d0b0a5576a2bc9aad
sharonobuobi <sharon.obuobi@gmail.com>
2024-12-01 14:32:51 -0500

    white loading background

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingScreen = () => {
   return (
-    <div className="fixed inset-0 flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900">
+    <div className="fixed inset-0 flex flex-col items-center justify-center bg-white">
       <div className="flex flex-col items-center">
         <div className="animate-pulse">
             <img src={require('../assets/images/AltFinance_Logo.png')} alt="AltFinance Logo" className="w-48 h-auto" />
